e34e2a6a2b00b7e5a920ac6c7b1e05fe11b4e1a1
Minsu Kim <alstn9008@naver.com>
2020-11-23 13:31:56 +0900

    add start, end, label location

--- MyHelloPanelListener.java
+++ MyHelloPanelListener.java
@@ -7,46 +7,65 @@ import java.awt.event.*;
  * @author (2017210105 김민수, 2019315066 사사키야스오)
  * @version (2020.11.23)
  */
-public class MyHelloPanelListener extends JPanel implements MouseListener, MouseMotionListener
+public class MyHelloPanelListener extends JPanel implements MouseListener, MouseMotionListener, KeyListener
 {
     JLabel ml;
     public MyHelloPanelListener()
     {
-        ml = new JLabel("No Mouse Event");
+        ml = new JLabel("HELLO");
+        this.setLayout(null);
+        ml.setLocation(50, 50);
+        ml.setSize(100, 20);
         this.add(ml);
 
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
+        this.addKeyListener(this);
     }
 
     public void mouseEntered(MouseEvent e)
     {
-        ml.setText("mouseEntered(" + e.getX() + ", " + e.getY() + ")");
+        ml.setText("Start");
         this.setBackground(Color.YELLOW);
     }
 
     public void mouseExited(MouseEvent e)
     {
-        ml.setText("mouseExited(" + e.getX() + ", " + e.getY() + ")");
+        ml.setText("End");
         this.setBackground(Color.GRAY);
     }
 
-    public void mouseMoved(MouseEvent e)
+    public void keyPressed(KeyEvent e)
     {
-        ml.setText("mouseMoved(" + e.getX() + ", " + e.getY() + ")");
+        int keyCode = e.getKeyCode();
+        switch(keyCode)
+        {
+            case KeyEvent.VK_UP:
+            ml.setLocation(ml.getX(), ml.getY() - 10);
+            break;
+            case KeyEvent.VK_DOWN:
+            ml.setLocation(ml.getX(), ml.getY() + 10);
+            break;
+            case KeyEvent.VK_LEFT:
+            ml.setLocation(ml.getX() - 10, ml.getY());            
+            break;
+            case KeyEvent.VK_RIGHT:
+            ml.setLocation(ml.getX() + 10, ml.getY());            
+            break;
+        }
     }
 
-    public void mouseDragged(MouseEvent e)
-    {
-        ml.setText("mouseDragged(" + e.getX() + ", " + e.getY() + ")");
-    }
+    public void keyReleased(KeyEvent e){}
 
-    public void mouseReleased(MouseEvent e)
-    {
-        ml.setText("mouseReleased(" + e.getX() + ", " + e.getY() + ")");
-    }
+    public void keyTyped(KeyEvent e){}
+
+    public void mouseMoved(MouseEvent e){}
+
+    public void mouseDragged(MouseEvent e){}
+
+    public void mouseReleased(MouseEvent e){}
 
     public void mousePressed(MouseEvent e){}
-    
+
     public void mouseClicked(MouseEvent e){}
 }
