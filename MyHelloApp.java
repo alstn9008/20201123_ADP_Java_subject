c0546acac17f1ef5a2fd7c39175cd240c29e6ca5
Minsu Kim <alstn9008@naver.com>
2020-11-23 12:42:15 +0900

    Initial sharing of project

---
+++ MyHelloApp.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class MyHelloApp here.
+ *
+ * @author (2017210105 김민수, 2019315066 사사키야스오)
+ * @version (2020.11.23)
+ */
+public class MyHelloApp
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ MyHelloFrame.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class MyHelloApp here.
+ *
+ * @author (2017210105 김민수, 2019315066 사사키야스오)
+ * @version (2020.11.23)
+ */
+public class MyHelloFrame
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MyHelloFrame
+     */
+    public MyHelloFrame()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
